5b64d421313d11dbd216468981fddde4601d3141
dipa333333 <putujayadipa@gmail.com>
2025-11-11 18:50:20 +0800

    update

--- resources/views/pages/tentang.blade.php
+++ resources/views/pages/tentang.blade.php
@@ -9,6 +9,7 @@
         <div class="card-body">
             <p>Aplikasi ini dibuat menggunakan Laravel + Bootstrap 5.</p>
             <p>Dikembangkan oleh anak agung putu jaya dipa  sebagai mini project pembelajaran dan sebagai project uts yang kemungkinan juga akan dikembangkan sebagai project uas Pemrograman Web Framework nantinya.</p>
+            <p>Project ini telah menerapkan berbagai konsep dalam laravel, seperti controller, routing, layout/templating, pagination, dan migration ke database menggunakan mysql</p>
         </div>
     </div>
 </div>
